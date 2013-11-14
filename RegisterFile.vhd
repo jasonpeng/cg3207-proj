a01f70442c26464fcc53d5701522e10a97b2d222
jasonpeng <jasonpeng.boy@gmail.com>
2013-11-15 00:34:47 +0800

    bug fix in RegisterFile

--- RegisterFile.vhd
+++ RegisterFile.vhd
@@ -12,7 +12,16 @@ entity RegisterFile is
 		RegAddr_1  : in STD_LOGIC_VECTOR(4 downto 0);
 		RegAddr_2  : in STD_LOGIC_VECTOR(4 downto 0);
 		RegData_1  : out STD_LOGIC_VECTOR(31 downto 0);
-		RegData_2  : out STD_LOGIC_VECTOR(31 downto 0)
+		RegData_2  : out STD_LOGIC_VECTOR(31 downto 0);
+		
+		Reg_1 : out STD_LOGIC_VECTOR(31 downto 0);
+		Reg_2 : out STD_LOGIC_VECTOR(31 downto 0);
+		Reg_3 : out STD_LOGIC_VECTOR(31 downto 0);
+		Reg_4 : out STD_LOGIC_VECTOR(31 downto 0);
+		Reg_5 : out STD_LOGIC_VECTOR(31 downto 0);
+		Reg_6 : out STD_LOGIC_VECTOR(31 downto 0);
+		Reg_7 : out STD_LOGIC_VECTOR(31 downto 0);
+		Reg_8 : out STD_LOGIC_VECTOR(31 downto 0)
 	);
 end RegisterFile;
 
@@ -20,15 +29,25 @@ architecture beh of RegisterFile is
 	TYPE rf is array (0 to 31) of std_logic_vector (31 downto 0);
 	signal rf_array: rf;
 begin
--- write data from falling_edge
-process(CLK, RESET, RegWrite, RegWriteAddr, RegWriteData)
+
+Reg_1 <= rf_array(1);
+Reg_2 <= rf_array(2);
+Reg_3 <= rf_array(3);
+Reg_4 <= rf_array(4);
+Reg_5 <= rf_array(5);
+Reg_6 <= rf_array(6);
+Reg_7 <= rf_array(7);
+Reg_8 <= rf_array(8);
+
+-- write data at rising_edge
+process(RESET, RegWrite, RegWriteAddr, RegWriteData)
 	variable index_write : integer range 0 to 31;
 begin
 	if (RESET='1') then
 		for i in 0 to 31 loop
 			rf_array(i) <= (others => '0');
 		end loop;
-	elsif falling_edge(CLK) then
+	else
 		index_write := to_integer(unsigned(RegWriteAddr));	
 		if (RegWrite='1') then
 			rf_array(index_write) <= RegWriteData;
@@ -36,7 +55,7 @@ begin
 	end if;
 end process;
 
--- read data from rising edge
+-- read data at falling_edge
 process(CLK, RESET, RegAddr_1, RegAddr_2)
 	variable index_1 : integer range 0 to 31;
 	variable index_2 : integer range 0 to 31;
@@ -44,7 +63,7 @@ begin
 	if (RESET='1') then
 		RegData_1 <= (others => 'Z');
 		RegData_2 <= (others => 'Z');
-	elsif rising_edge(CLK) then
+	elsif falling_edge(CLK) then
 		index_1 := to_integer(unsigned(RegAddr_1));
 		index_2 := to_integer(unsigned(RegAddr_2));
 		RegData_1 <= rf_array(index_1);
