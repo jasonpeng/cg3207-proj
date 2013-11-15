bc3a564401117b5b3ef26cf6947b18bc3be9f52a
jasonpeng <jasonpeng.boy@gmail.com>
2013-11-15 16:10:54 +0800

    updated ASM code

--- rom-test.asm
+++ rom-test.asm
@@ -1,34 +1,22 @@
-ori $1,$0, 0
-ori $1,$0, 1
-.label2: ori $2,$0, 2
-ori $3,$0, 4
-ori $4,$0, 8
+.data
+0, 1, 2, 3, 4, 5, 6, 7
+
+.text
+ori $2, $0, 3
+ori $3, $0, 9
+add $4, $2, $3
+add $4, $4, $2
+add $4, $4, $3
+lw $5, 4
+add $4, $4, $5
+sw $4, 8
 nop
-nop
-nop
-nor $5,$1,$2
-nop
-add $6,$1,$2
-nop
-and $7,$3,$4
-nop
-sub $8,$4,$3
-nop
-sw $5, 0
-nop
-nop
-nop
-lw $8, 0
-nop
-nop
-nop
-j .label
-nop
-nop
-nop
-nop
-.label:  slt $8, $1, $2
-beq $11, $12 .label2
-nop
-nop
-nop
+lw $6, 8
+slt $7, $4, $6
+slti $8, $2, 4
+sll $8, $3, 7
+sllv $5, $2, $3
+sra $7, $3, 1
+srav $7, $3, $2
+srl $8, $3, 1
+srlv $8, $3, $2
